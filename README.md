87c1b67942e6f0d18fe493634f3db141a4c16bd0
Marko Starjački <mstarjac@gmail.com>
2021-01-10 19:46:06 +0100

    inital commit
    Signed-off-by: Marko Starjački <mstarjac@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# qaAppium
+## QA Task
+This repository contains a simple test for RoboKiller Anroid version. iOS version was not made due to not owning an Mac, but since [Appium](http://appium.io/) is a multi platform framework, it is easy to extend the current test to support iOS devices by adding an iOS WebDriver (e.g. [UIAutomation](http://appium.io/docs/en/drivers/ios-uiautomation/index.html))
+
+### Test description
+* Installs RoboKiller Anroid version from this repository
+* Enables call filtering for numbers not on contact list
+* Simulates phone call
+* Checks if previous call was blocked
+* Adds previous number to contacts
+* Retries call and check if call was blocked
+* Removes number from contacts
+
+## Tools/Frameworks used
+As mentioned previously, this test uses [Appium](http://appium.io/) as the main testing framework. Appium is used because it's one of the top open-source frameworks for mobile test automation, it supports both Android and iOS devices, has a well written documentation and multiple programming languages supported (e.g. Java, Python).
+
+For this test specifically, Appium desktop was used because of it's ease of use and setup, but for a more advanced setup for CI integration, `npm` version should be used instead.
+
+###  UiAutomator2 and Junit5
+There is no specific reason for using UiAutomator2 and Junit5 besides that the tutorial I watched was using the same. In addition UiAutomator2 is supported by Firebase.
+
+### IntelliJ IDEA (Community Edition)
+Easy to use IDE with out of the box support for all tools needed to run Appium tests.
+ 
+## Requirements for the project
+* Android SDK 28+
+* Java 8+ 
+* Android emulator (real device does not support appium GSM calls)
+  * Android version 11 
+
+## What was not done and why
+The given time limit was enough to complete the task even with the "extra credit", but with all the events that were happening, available time I had at my hands was limited.
+
+* Multiple functional tests
+  * Only one test was written because any others would be done on the same manner, but just selecting a different filter option 
+* Unit test
+  * I did lookup how to write one, and I would have used JUnit5 in same manner as the functional one
+* Integration tests/Screenshot tests
+  * I did not lookup  
+* Firebase Test Lab usage
+  * Made only minor check, of possible options on how to setup project with firebase
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,144 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.GsmCallActions;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FirstTest {
+
+    private static final String APP = "https://github.com/Megued/qaAppium/raw/main/app-debug.apk";
+    private static final String APPIUM = "http://localhost:4723/wd/hub";
+    private static final int timeOut = 5;
+    private static final String phoneNumber = "1234567890";
+
+    private static AndroidDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "11");
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("app", APP);
+        driver = new AndroidDriver(new URL(APPIUM), caps);
+        wait = new WebDriverWait(driver, timeOut);
+        
+        // used to check if application loaded
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("co.teltech.callblocker:id/counterBlockedCalls")));
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @DisplayName("Enable blocking calls")
+    @Order(1)
+    public void enableBlock() {
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("co.teltech.callblocker:id/switchBlockCalls"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.permissioncontroller:id/permission_allow_button"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.permissioncontroller:id/permission_allow_button"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("co.teltech.callblocker:id/radioOptionNotContacts"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.permissioncontroller:id/permission_allow_button"))).click();
+    }
+
+    @Test
+    @DisplayName("Make call from fake number")
+    @Order(2)
+    public void callFromFakeNumber() throws IOException, InterruptedException {
+        // wait for block to be enabled
+        wait.until(ExpectedConditions.attributeToBe(MobileBy.id("co.teltech.callblocker:id/radioOptionNotContacts"),"checked", "true"));
+        driver.makeGsmCall(phoneNumber, GsmCallActions.CALL);
+        Thread.sleep(2000);
+
+        WebElement call = driver.findElement(MobileBy.id("co.teltech.callblocker:id/counterBlockedCalls"));
+
+        if(call.isDisplayed())
+            call.click();
+
+        File scrFile =  ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("incomingCall.png"));
+    }
+
+    @Test
+    @DisplayName("Check last call was blocked")
+    @Order(3)
+    public void checkBlockedCall(){
+        wait.until(ExpectedConditions.attributeToBe(MobileBy.id("co.teltech.callblocker:id/counterBlockedCalls"),"text", "1"));
+    }
+
+    @Test
+    @DisplayName("Add number to contacts")
+    @Order(4)
+    public void addNumberToContacts() {
+        driver.startActivity(new Activity("com.android.dialer", "main.impl.MainActivity"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.dialer:id/call_log_tab"))).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.dialer:id/primary_action_view"))).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.dialer:id/create_new_contact_action"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Name")));
+
+        List<WebElement> contact = driver.findElements(MobileBy.className("android.widget.EditText"));
+        contact.get(0).sendKeys("TestName");
+        driver.findElement(MobileBy.id("com.android.contacts:id/editor_menu_save_button")).click();
+    }
+
+    @Test
+    @DisplayName("Check if contact number is not blocked")
+    @Order(5)
+    public void checkIfContactNumberIsBlocked() throws InterruptedException {
+        driver.startActivity(new Activity("co.teltech.callblocker", "activities.SplashActivity"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("co.teltech.callblocker:id/counterBlockedCalls")));
+        driver.makeGsmCall(phoneNumber, GsmCallActions.CALL);
+
+        // wait for call to go trough
+        Thread.sleep(2000);
+
+        WebElement call = driver.findElement(MobileBy.id("co.teltech.callblocker:id/counterBlockedCalls"));
+
+        if(call.isDisplayed())
+            call.click();
+
+        wait.until(ExpectedConditions.attributeToBe(MobileBy.id("co.teltech.callblocker:id/counterBlockedCalls"),"text", "1"));
+    }
+
+    @Test
+    @DisplayName("Remove number from contact")
+    @Order(6)
+    public void removeFromContact() throws InterruptedException {
+        driver.startActivity(new Activity("com.android.dialer", "main.impl.MainActivity"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("com.android.dialer:id/contacts_tab"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Quick contact for TestName"))).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("More options"))).click();
+
+        // use sleep for popup to open, rather than wait by xpath
+        Thread.sleep(2000);
+
+        List<WebElement> optionsList = driver.findElements(MobileBy.className("android.widget.LinearLayout"));
+        optionsList.get(2).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.id("android:id/button1"))).click();
+    }
+}
